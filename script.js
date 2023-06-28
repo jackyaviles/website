fd53aa16e497da477119633dd51a8dc98411b118
jackyaviles <138074691+jackyaviles@users.noreply.github.com>
2023-06-28 18:18:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var myName = "jackie"
+myName = 1; 
+console.log(myName);
+var haspets = true; 
+
+
+
+console.log(haspets); 
+
+     var x = 0; 
+     while (x < 5){
+        console.log(" im so tired ") ; 
+         x++  
+     }
+         
+var button = document.getElementById 
+var h1 = document.documentElementById("nails")
+var body = document.body;
+
+buttton.addEvent.listener ("click" function" )()
+      h1.innerHTML = "i wanna go home 
